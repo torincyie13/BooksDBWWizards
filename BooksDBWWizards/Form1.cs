9638e33f5412f1a582ad8e8bfb5100fc649e1129
Torin Hawkins <torincyie13@gmail.com>
2021-06-10 20:34:14 -0500

    Add project files.

---
+++ BooksDBWWizards/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BooksDBWWizards
+{
+	public partial class frmTitles : Form
+	{
+		public frmTitles()
+		{
+			InitializeComponent();
+		}
+
+		private void titlesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+		{
+			this.Validate();
+			this.titlesBindingSource.EndEdit();
+			this.tableAdapterManager.UpdateAll(this.sQLBooksDBDataSet);
+
+		}
+
+		private void titlesBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+		{
+			this.Validate();
+			this.titlesBindingSource.EndEdit();
+			this.tableAdapterManager.UpdateAll(this.sQLBooksDBDataSet);
+
+		}
+
+		private void frmTitles_Load(object sender, EventArgs e)
+		{
+			// TODO: This line of code loads data into the 'sQLBooksDBDataSet.Titles' table. You can move, or remove it, as needed.
+			this.titlesTableAdapter.Fill(this.sQLBooksDBDataSet.Titles);
+
+		}
+	}
+}
